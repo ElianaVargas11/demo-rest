bb5c9467205a6bf8140506ee5b07e844d966559f
Eliana Vargas Rojas <evargasrojas11@gmail.com>
2021-05-12 18:26:00 -0400

    Veterinario

---
+++ src/main/java/bo/edu/ucb/ingsoft/demorest/Veterinario.java
@@ -0,0 +1,17 @@
+package bo.edu.ucb.ingsoft.demorest;
+
+public class Veterinario {
+       public Integer idVeterinario;
+       public Integer idUsuario;
+       public Integer idVeterinaria;
+       public Integer idImagen;
+       public String nombre;
+       public String apellido;
+       public String email;
+       public String departamento;
+       public String lugarFormacion;
+
+       public Veterinario(){
+
+       }
+}
